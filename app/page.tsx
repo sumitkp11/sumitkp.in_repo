866746835b8c9459e24dcebbe3af8e7de81c6f8f
Sumit Prasad <sumitk11@hotmail.com>
2024-12-14 23:57:32 +0530

    add privacy route

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import LikeButton from "./like-button";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <LikeButton />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
