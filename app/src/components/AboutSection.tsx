2b34be20c49f965a2d968459a79a1e669b361631
Sumit Prasad <sumitk11@hotmail.com>
2025-01-09 14:30:04 +0530

    modify header and footer styling and UX

--- app/src/components/AboutSection.tsx
+++ app/src/components/AboutSection.tsx
@@ -18,16 +18,18 @@ function AboutSection() {
         };
     }, []);
     return (
-        <div className="relative h-dvh bg-gray-100 md:columns-2 md:flex-col flex-row">
-            <div className="md:order-1 order-last w-100 pt-8 md:h-full md:flex flex-col justify-center items-center">
-                <img className="w-1/2 md:w-100% mx-auto rounded-full md:drop-shadow-xl" src="./sumit-1.png" />
+        <div>
+            <div className="relative h-screen bg-gray-100 md:columns-2 md:flex-col flex-row">
+                <div className="md:order-1 order-last w-100 pt-8 md:h-full md:flex flex-col justify-center items-center">
+                    <img className="w-1/2 md:w-100% mx-auto rounded-full md:drop-shadow-2xl" src="./sumit-1.png" />
 
-            </div>
-            <div className="md:order-last order-1 md:w-100 md:h-full md:flex md:justify-center md:items-center">
-                <div className="font-sans md:text-7xl">
-                    I am <span ref={el} />
                 </div>
+                <div className="md:order-last order-1 md:w-100 md:h-full md:flex md:justify-center md:items-center">
+                    <div className="font-sans md:text-7xl">
+                        I am <span ref={el} />
+                    </div>
 
+                </div>
             </div>
         </div>
     )
