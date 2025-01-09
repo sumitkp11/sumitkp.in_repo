988fd25f7bb856f14b1ae7df703332afe59ed56f
Sumit Prasad <sumitk11@hotmail.com>
2025-01-10 02:09:40 +0530

    add mobile video for nagarro page

--- app/jobs/nagarro/page.tsx
+++ app/jobs/nagarro/page.tsx
@@ -6,10 +6,14 @@ export default function NagarroPage() {
     <div>
       <Header />
       <div className="relative w-full h-[calc(100vh-4rem)] overflow-hidden">
-        <video autoPlay muted loop className="w-full h-auto sticky">
-          <source 
+        <video autoPlay muted loop className="hidden md:block w-full h-auto">
+          <source
             src="/videos/nagarro-web-bg.webm"
-          />          
+          />
+        </video>
+        <video autoPlay muted loop className="md:hidden w-full h-auto">
+          <source
+            src="/videos/nagarro-mobile-bg.webm" />
         </video>
         <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-30">
         </div>
