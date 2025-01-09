5c6eb0613742a7696ba83f5c276752cd8c9ebd52
Sumit Prasad <sumitk11@hotmail.com>
2025-01-09 15:10:17 +0530

    add hamburger menu to header for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="smooth-scroll">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
