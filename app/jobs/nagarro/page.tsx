daed2e9345805ee14cbb10406a014f2b171e188e
Sumit Prasad <sumitk11@hotmail.com>
2025-01-10 01:52:31 +0530

    add  web video for nagarro page

--- app/jobs/nagarro/page.tsx
+++ app/jobs/nagarro/page.tsx
@@ -5,7 +5,17 @@ export default function NagarroPage() {
   return (
     <div>
       <Header />
-      
+      <div className="relative w-full h-[calc(100vh-4rem)] overflow-hidden">
+        <video autoPlay muted loop className="w-full h-auto sticky">
+          <source 
+            src="/videos/nagarro-web-bg.webm"
+          />          
+        </video>
+        <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-30">
+        </div>
+      </div>
+      <div className="bg-purple-200 h-screen"></div>
+
       <FooterSection />
     </div>
 
